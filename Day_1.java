e7160fe584b27bbeef088fc6c362af3cc668533b
olivia1906-dev <ishikag.1906@gmail.com>
2026-01-18 23:11:28 +0530

    Basic Arithmetic operations

--- Day_1.java
+++ Day_1.java
@@ -1,5 +1,23 @@
 public class Day_1 {
-    static void main() {
-        system.out.print("Ishika Gupta");
+    public static void main(String[] args) {
+        System.out.println("Basic calculator perfroming basic operations like additon, subtraction, multiplication and division.")
+        // Using literals (fixed values)
+        int num1 = 25;
+        int num2 = 5;
+
+        // Performing calculations
+        int addition = num1 + num2;
+        int subtraction = num1 - num2;
+        int multiplication = num1 * num2;
+        int division = num1 / num2;
+
+        // Displaying results
+        System.out.println("First Number: " + num1);
+        System.out.println("Second Number: " + num2);
+
+        System.out.println("Addition = " + addition);
+        System.out.println("Subtraction = " + subtraction);
+        System.out.println("Multiplication = " + multiplication);
+        System.out.println("Division = " + division);
     }
 }
