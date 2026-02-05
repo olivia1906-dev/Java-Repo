03598754456f3d4720e917ce1fc5e66b7a3ce2f7
ISHIKA-GUPTA <ishikag.1906@gmail.com>
2026-02-05 20:59:55 +0530

    Create Day_19.java

---
+++ Day_19.java
@@ -0,0 +1,24 @@
+30 - Day Java Challenge | Day -19 : Smart Billing System
+
+Just completed a beginner-level **Java program** to strengthen my fundamentals 💻
+
+### What the program does:
+
+* Takes item price and quantity from the user
+* Calculates the total bill using arithmetic operators
+* Applies discount using **nested if-else**:
+
+  * 5% discount if bill ≥ 2000
+  * 10% discount if bill ≥ 5000
+* Adds **18% GST** on the discounted amount
+* Displays the final payable amount
+
+### Concepts practiced:
+
+* Arithmetic operators
+* Relational operators
+* Nested `if-else`
+* User input using `Scanner`
+
+
+#Java #JavaBeginner #Programming #LearningJava #CodingPractice #StudentDeveloper #IfElse #CoreJava #Day19
